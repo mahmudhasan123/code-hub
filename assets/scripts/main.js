29261c81e72b4a74d45eb90ee2801fb3edbbd0ff
mahmudhasan123 <mahmudcodes@gmail.com>
2025-03-02 09:05:50 +0600

    add homepage ui

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+const todayDate = new Date();
+const formatDate = todayDate.toDateString();
+
+document.getElementById('currentDay').innerHTML = formatDate;
+
+document.getElementById('goToBlog').addEventListener('click', function() {
+    window.location.href = 'blog.html'; 
+  });
